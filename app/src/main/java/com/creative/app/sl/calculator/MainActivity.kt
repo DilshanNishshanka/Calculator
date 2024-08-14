7be2fecc72a3a242864b03f358246504ad1b5da0
D i l s h a n <binoddilshan@gmail.com>
2024-08-15 00:51:44 +0530

    Develop a Basic Calculator app (Version 1)

---
+++ app/src/main/java/com/creative/app/sl/calculator/MainActivity.kt
@@ -0,0 +1,115 @@
+package com.creative.app.sl.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.Calculator
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.creative.app.sl.calculator.ui.theme.CalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Calculator()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Calculator(){
+
+    var numberOne by remember { mutableStateOf("") }
+    var numberTwo by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+
+    fun sumCalculation(){
+        val inputValueNumberOne = numberOne.toIntOrNull() ?: 0
+        val inputValueNumberTwo = numberTwo.toIntOrNull() ?: 0
+        val result = inputValueNumberOne + inputValueNumberTwo
+        outputValue = result.toString()
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row {
+            OutlinedTextField(
+                value = numberOne,
+                onValueChange = {
+                      numberOne = it
+                      sumCalculation()
+                },
+                label = {
+                    Text(
+                        text = "Enter First Number"
+                    )
+                }
+            )
+        }
+        Row {
+           Text(
+               text = "+"
+           )
+        }
+        Row {
+            OutlinedTextField(
+                value = numberTwo,
+                onValueChange = {
+                     numberTwo = it
+                     sumCalculation()
+                },
+                label = {
+                    Text(
+                        text = "Enter Second Number"
+                    )
+                }
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Text(
+                text = outputValue
+            )
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+@Composable
+fun CalculatorPreview() {
+    CalculatorTheme {
+       Calculator()
+    }
+}
