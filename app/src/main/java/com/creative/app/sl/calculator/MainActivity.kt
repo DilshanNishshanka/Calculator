8788fd9e3fc641642f0b67a0af217ecf2782b4ee
D i l s h a n <binoddilshan@gmail.com>
2024-08-22 00:27:22 +0530

    Remove Unused Import

--- app/src/main/java/com/creative/app/sl/calculator/MainActivity.kt
+++ app/src/main/java/com/creative/app/sl/calculator/MainActivity.kt
@@ -3,7 +3,6 @@ package com.creative.app.sl.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
