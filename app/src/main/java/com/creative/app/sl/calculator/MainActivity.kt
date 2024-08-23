1b6d12e601214f1372c7c7a60c0ce2bdceb010e1
D i l s h a n <binoddilshan@gmail.com>
2024-08-23 23:45:47 +0530

    Add font Weight for tab layout

--- app/src/main/java/com/creative/app/sl/calculator/MainActivity.kt
+++ app/src/main/java/com/creative/app/sl/calculator/MainActivity.kt
@@ -29,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -168,12 +169,14 @@ fun Calculator() {
                         if (selected) Text(
                             text = title,
                             color = Color.Red,
-                            fontSize = 20.sp
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold
                         )
                         else Text(
                             text = title,
                             color = Color.Black,
-                            fontSize = 20.sp
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Light
                         )
                     } ,
                     selected = selected,
